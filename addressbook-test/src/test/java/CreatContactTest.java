99a44921619f39e57858dc5b596a1a0507225ff0
goldberalex <33997546+goldberalex@users.noreply.github.com>
2018-01-04 14:23:56 +0200

    1235в

--- addressbook-test/src/test/java/CreatContactTest.java
+++ addressbook-test/src/test/java/CreatContactTest.java
@@ -6,7 +6,7 @@ public class CreatContactTest extends TestBase {
     @Test
     public void CreatContactTest() {
         initContactCreation();
-        fillContactForm();
+        fillContactForm();  
         submitContactCreation();
 
     }
