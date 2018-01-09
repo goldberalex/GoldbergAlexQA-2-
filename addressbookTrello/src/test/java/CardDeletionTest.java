7c85a9e5a351858d280aec59ba473798d16c97bf
goldberalex <33997546+goldberalex@users.noreply.github.com>
2018-01-09 20:34:20 +0200

    addressbookTrello

--- addressbookTrello/src/test/java/CardDeletionTest.java
+++ addressbookTrello/src/test/java/CardDeletionTest.java
@@ -4,8 +4,11 @@ import org.testng.annotations.Test;
 public class CardDeletionTest extends TestBase{
     @Test
     public void cardDeletionTest(){
+        //int before = wd.findElements(By.).size();
         selectCard();
         clickButtonArchive();
+        //int after = wd.findElements(By.xpath()).size();
+        //Assert.assertEquals(after, before-1);
 
     }
 
