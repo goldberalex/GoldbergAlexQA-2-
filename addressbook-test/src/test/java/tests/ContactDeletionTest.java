099e14a7a6217b8a57cbff93efeae092273bbc29
goldberalex <33997546+goldberalex@users.noreply.github.com>
2018-01-28 10:12:29 +0200

    List<ContactData>

--- addressbook-test/src/test/java/tests/ContactDeletionTest.java
+++ addressbook-test/src/test/java/tests/ContactDeletionTest.java
@@ -4,6 +4,8 @@ import model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 
 public class ContactDeletionTest extends TestBase{
     @Test//(enabled = false)//(enabled = false) означает что этот тест не запускать
@@ -17,17 +19,22 @@ public class ContactDeletionTest extends TestBase{
                     .wihtHome("BR2")
                     .wihtAddress("BR2"));
         }
-        int before = app.getContactHelper().getContactCout();
-        app.getContactHelper().selecContactByIndex(before-1);
+        List<ContactData> before = app.getContactHelper().getContactList();
+        //int before = app.getContactHelper().getContactCout();
+        app.getContactHelper().selecContactByIndex(before.size()-1);
         //выделяем строчку wd.findElements(By.name("selected[]")).size();
         // далее ctrl->alt-> называем этот метод getContactCoutDeletion, он спросит применить к остальным?,
         // нажимаем кнопку ОК и далее метод что появится ниже перетаскиваем его в ApplicationManager
         //app.getContactHelper().selectContact();
         app.getContactHelper().clickButtonDeleteContact();
         //app.getContactHelper().confirmAlert(); //Alert нужен если в selectContact() будет click(By.name("selected[]"));
-        int after = app.getContactHelper().getContactCout();
-        Assert.assertEquals(after, before-1);
+       List<ContactData> after = app.getContactHelper().getContactList();
+        //int after = app.getContactHelper().getContactCout();
+        Assert.assertEquals(after.size(), before.size()-1);
+
+        before.remove(before.size()-1);
+        for (int i=0; i < before.size(); i++){
+            Assert.assertEquals(before.get(i),after.get(i));
+        }
     }
-
-
 }
